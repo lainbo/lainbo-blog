852c511767858dab1d44bc4c3f729c845e900230
lainbo <lainbo@foxmail.com>
2023-04-23 20:42:09 +0800

    refactor: ♻️ improve plum style

--- components/BranchingLines.js
+++ components/BranchingLines.js
@@ -1,39 +1,69 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useEffect, useState } from 'react';
 
 const BranchingLines = () => {
   const canvasRef = useRef(null);
-  const [mask, setMask] = useState("radial-gradient(circle, transparent, black);");
+  const [mask, setMask] = useState('');
+
+  useEffect(() => {
+    setMask('radial-gradient(circle, transparent, black)');
+  }, []);
 
   useEffect(() => {
     const r180 = Math.PI;
     const r90 = Math.PI / 2;
     const r15 = Math.PI / 12;
-    const color = "#88888825";
-    const random = Math.random;
-    let lastTime = performance.now();
-    const interval = 1000 / 40;
-    let animationId;
+    const color = '#88888825';
+    const el = canvasRef.current;
+    const { random } = Math;
 
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      const size = { width: window.innerWidth, height: window.innerHeight };
+    const size = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    const initCanvas = (canvas, width = 400, height = 400, _dpi) => {
+      const ctx = canvas.getContext('2d');
+
       const dpr = window.devicePixelRatio || 1;
+      const bsr =
+        ctx.webkitBackingStorePixelRatio ||
+        ctx.mozBackingStorePixelRatio ||
+        ctx.msBackingStorePixelRatio ||
+        ctx.oBackingStorePixelRatio ||
+        ctx.backingStorePixelRatio ||
+        1;
 
-      canvas.style.width = `${size.width}px`;
-      canvas.style.height = `${size.height}px`;
-      canvas.width = dpr * size.width;
-      canvas.height = dpr * size.height;
-      ctx.scale(dpr, dpr);
+      const dpi = _dpi || dpr / bsr;
 
-      const polar2cart = (x = 0, y = 0, r = 0, theta = 0) => {
-        const dx = r * Math.cos(theta);
-        const dy = r * Math.sin(theta);
-        return [x + dx, y + dy];
-      };
+      canvas.style.width = `${width}px`;
+      canvas.style.height = `${height}px`;
+      canvas.width = dpi * width;
+      canvas.height = dpi * height;
+      ctx.scale(dpi, dpi);
+
+      return { ctx, dpi };
+    };
+
+    const polar2cart = (x = 0, y = 0, r = 0, theta = 0) => {
+      const dx = r * Math.cos(theta);
+      const dy = r * Math.sin(theta);
+      return [x + dx, y + dy];
+    };
+
+    const init = 4;
+    const len = 6;
+
+    if (el) {
+      const { ctx } = initCanvas(el, size.width, size.height);
+      const { width, height } = el;
+
+      let steps = [];
+      let prevSteps = [];
+
+      let iterations = 0;
 
       const step = (x, y, rad) => {
-        const length = random() * 6;
+        const length = random() * len;
 
         const [nx, ny] = polar2cart(x, y, length, rad);
 
@@ -48,58 +78,55 @@ const BranchingLines = () => {
         if (nx < -100 || nx > size.width + 100 || ny < -100 || ny > size.height + 100)
           return;
 
-        if (random() > 0.5) steps.push(() => step(nx, ny, rad1));
-        if (random() > 0.5) steps.push(() => step(nx, ny, rad2));
+        if (iterations <= init || random() > 0.5)
+          steps.push(() => step(nx, ny, rad1));
+        if (iterations <= init || random() > 0.5)
+          steps.push(() => step(nx, ny, rad2));
       };
 
-
-      let steps = [];
-      let prevSteps = [];
+      let lastTime = performance.now();
+      const interval = 1000 / 40;
 
       const frame = () => {
         if (performance.now() - lastTime < interval) {
-          animationId = requestAnimationFrame(frame);
+          requestAnimationFrame(frame);
           return;
         }
 
+        iterations += 1;
         prevSteps = steps;
         steps = [];
         lastTime = performance.now();
 
         if (!prevSteps.length) {
-          cancelAnimationFrame(animationId);
+          cancelAnimationFrame(requestAnimationFrame(frame));
         }
-        prevSteps.forEach((i) => i());
+        prevSteps.forEach(i => i());
 
-        animationId = requestAnimationFrame(frame);
+        lastTime = performance.now();
+        requestAnimationFrame(frame);
       };
 
       const start = () => {
-        if (animationId) {
-          cancelAnimationFrame(animationId);
-        }
-
-        ctx.clearRect(0, 0, size.width, size.height);
+        iterations = 0;
+        ctx.clearRect(0, 0, width, height);
         ctx.lineWidth = 1;
         ctx.strokeStyle = color;
+        prevSteps = [];
         steps = [
           () => step(random() * size.width, 0, r90),
           () => step(random() * size.width, size.height, -r90),
           () => step(0, random() * size.height, 0),
           () => step(size.width, random() * size.height, r180),
         ];
-        if (size.width < 500) {
+        if (size.width < 500)
           steps = steps.slice(0, 2);
-        }
-        animationId = requestAnimationFrame(frame);
+        requestAnimationFrame(frame);
       };
 
       start();
-      window.addEventListener("resize", start);
-      return () => {
-        window.removeEventListener("resize", start);
-        cancelAnimationFrame(animationId);
-      };
+      window.addEventListener('resize', start);
+      return () => window.removeEventListener('resize', start);
     }
   }, [mask]);
 
@@ -118,4 +145,3 @@ const BranchingLines = () => {
 };
 
 export default BranchingLines;
-
